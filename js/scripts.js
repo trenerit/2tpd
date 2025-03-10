1aee8723893aa96d486ef9e15bb9b0e158a4c3ae
Piotr Kania <piotr@trener-it.pl>
2025-03-10 13:30:14 +0100

    update modal styles and add box-offer

--- js/scripts.js
+++ js/scripts.js
@@ -16,10 +16,36 @@ window.addEventListener('scroll', navScroll);
 // modal window
 const divModal = document.querySelector('#modal');
 
-const btn1 = document.querySelector('article a');
+const btn = document.querySelectorAll('article a');
 
-btn1.addEventListener('click', () => {
+const modalContent = document.querySelector('.modal-content');
 
-    divModal.style.display = 'flex';
+const close = document.querySelector('.modal-content a');
 
+const articlesH3 = document.querySelectorAll('article h3');
+
+btn.forEach((elem, index) => {
+
+    const country = articlesH3[index].innerText;
+    console.log(country);
+
+    elem.addEventListener('click', (e, c = country) => {
+        e.preventDefault();
+        const modalH4 = document.querySelector('.modal-content h4');
+        modalH4.innerText = c;
+        divModal.style.display = 'flex';
+    });
 });
+
+divModal.addEventListener('click', () => {
+    divModal.style.display = 'none';
+});
+
+modalContent.addEventListener('click', (e) => {
+    e.stopPropagation();
+}); 
+
+close.addEventListener('click', (e) => {
+    e.preventDefault();
+    divModal.style.display = 'none';
+})
